7eed01090c56feca5fae6ad5ab7ec092c239a797
satya's office-laptop <narsss1234@gmail.com>
2024-02-12 16:54:50 +0530

    gunicorn

--- userdata_for_deploy.sh
+++ userdata_for_deploy.sh
@@ -2,9 +2,15 @@
 
 sudo apt update --fix-missing
 
-sudo apt install git python3 pip nginx -y
+sudo apt install git python3 pip nginx gunicorn -y
 
 git clone -b Jenkins https://github.com/CharismaticOwl/Graded-Assignment-on-CI-CD-Pipeline.git
+
+cd Graded-Assignment-on-CI-CD-Pipeline
+
+sudo pip install -r requirements.txt
+
+gunicorn --bind 0.0.0.0:3000 run:app
 
 sudo systemctl enable nginx
 
@@ -13,7 +19,7 @@ sudo systemctl start nginx
 cat <<EOT > pythonapp
 server {
     listen 80;
-    server_name _
+    server_name _;
     location / {
         proxy_pass http://127.0.0.1:3000;
     }
@@ -26,7 +32,3 @@ rm -rf /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/pythonapp /etc/nginx/sites-enabled/pythonapp
 
 sudo systemctl restart nginx
-
-cd Graded-Assignment-on-CI-CD-Pipeline
-
-python3 hello.py
