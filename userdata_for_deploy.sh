c8b4704175153bc5c254d7378b415b7d491227bb
satya's office-laptop <narsss1234@gmail.com>
2024-02-12 19:16:25 +0530

    userdata for deploy

--- userdata_for_deploy.sh
+++ userdata_for_deploy.sh
@@ -2,10 +2,16 @@
 
 sudo apt update --fix-missing
 
-sudo apt install git python3 pip nginx gunicorn -y
+sudo apt install git python3 pip nginx npm -y
 
 git clone -b Jenkins https://github.com/CharismaticOwl/Graded-Assignment-on-CI-CD-Pipeline.git
 
 cd Graded-Assignment-on-CI-CD-Pipeline
 
 sudo pip install -r requirements.txt
+
+sudo npm install pm2 -g
+
+sudo pm2 start app.py --interpreter=python3
+
+sudo ufw allow 3000
